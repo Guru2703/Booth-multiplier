807cb632f41b8b68bc8e0420ce8a51d01ed033f5
Guru2703 <142711244+Guru2703@users.noreply.github.com>
2025-03-14 14:24:07 +0530

    Update extend.v

--- extend.v
+++ extend.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.03.2025 23:56:22
-// Design Name: 
-// Module Name: extend
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module extend(
 input [7:0] M, output [15:0] eM,eM_bar,eM2,eM2_bar  
     );
