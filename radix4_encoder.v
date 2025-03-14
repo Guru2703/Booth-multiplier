c642fedfd928513a173c301c51ce7ba146f8672d
Guru2703 <142711244+Guru2703@users.noreply.github.com>
2025-03-14 14:24:31 +0530

    Update radix4_encoder.v

--- radix4_encoder.v
+++ radix4_encoder.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.03.2025 23:47:04
-// Design Name: 
-// Module Name: radix4_encoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module radix4_encoder(
 inp,out
     );
